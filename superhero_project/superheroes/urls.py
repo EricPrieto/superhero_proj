182162dd19a26426697b23f77d42226c0aa44363
EricPrieto <ep1043@hotmail.com>
2021-10-28 09:17:21 -0500

    Added code from videos 6-8

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
-
-from .import views
+from . import views
 
 app_name = 'superheroes'
 urlpatterns =[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail'),
+    path('new/', views.create, name='create')
 ]
